53ae0331958e5d7e387b14a7da130aa5cd83243e
Manasi Rane <ranemanasi13@gmail.com>
2026-06-09 12:11:00 +0530

    Set up MaterialApp with theme and HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping App',
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(130, 209, 252, 1),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
 }
