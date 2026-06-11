3f7dc992e50cdcef413885b5f1e5556b1ca11c32
Manasi Rane <ranemanasi13@gmail.com>
2026-06-11 11:34:32 +0530

    Update theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Shopping App',
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(199, 178, 209, 255),
         fontFamily: 'Lato',
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromRGBO(130, 209, 252, 1),
+          seedColor: Color.fromRGBO(213, 189, 241, 1),
+          primary: Color.fromRGBO(213, 189, 241, 1),
         ),
         inputDecorationTheme: const InputDecorationTheme(
           hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
