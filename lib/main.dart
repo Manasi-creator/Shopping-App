2dc33b5a70e475e4fc16c158e97ac564127ad672
Manasi Rane <ranemanasi13@gmail.com>
2026-06-15 12:22:09 +0530

    Implement CartProvider and integrate it into CartPage and ProductDetailsPage for managing cart functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/cart_provider.dart';
 import 'package:shopping_app/home_page.dart';
 
 void main() {
@@ -10,31 +12,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shopping App',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(199, 178, 209, 255),
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromRGBO(213, 189, 241, 1),
-          primary: Color.fromRGBO(213, 189, 241, 1),
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shopping App',
+        theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromARGB(199, 178, 209, 255),
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromRGBO(213, 189, 241, 1),
+            primary: Color.fromRGBO(213, 189, 241, 1),
+          ),
+          appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            titleMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            titleSmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          useMaterial3: true,
         ),
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
-        ),
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-          titleMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          titleSmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-        useMaterial3: true,
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
